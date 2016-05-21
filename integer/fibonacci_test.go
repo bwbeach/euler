6a8b5632951a945bf093242e50b5665dc4a4c064
Brian Beach <coder@beachfamily.net>
2016-05-21 09:36:37 -1000

    Add FibonacciGenerator

---
+++ integer/fibonacci_test.go
@@ -0,0 +1,13 @@
+package integer
+
+import "testing"
+
+func TestFibonacciGenerator(t *testing.T) {
+	fib := FibonacciGenerator()
+	for i, expected := range []int{ 1, 1, 2, 3, 5, 8, 13, 21 } {
+		actual := fib()
+		if expected != actual {
+			t.Errorf("iteration %v: expected %v but got %v", i, expected, actual)
+		}
+	}
+}
