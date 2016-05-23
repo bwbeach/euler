04d2ddb247e1c58546daeaa1a312583d64cae373
Brian Beach <coder@beachfamily.net>
2016-05-22 16:13:50 -1000

    Add comments

--- integer/intbag.go
+++ integer/intbag.go
@@ -2,22 +2,28 @@ package integer
 
 import "sort"
 
+// IntBag is a bag of integers
 type IntBag map[int]int
 
+// Creates a new, empty bag
 func NewIntBag() *IntBag {
 	m := map[int]int{}
 	return (*IntBag)(& m)
 }
 
+// internal function that returns the map that represents a bag
 func (bag *IntBag) asMap() *map[int]int {
 	return (*map[int]int)(bag)
 }
 
+// Adds a number to a bag
 func (bag *IntBag) Add(n int) {
 	m := bag.asMap()
 	(*m)[n] = (*m)[n] + 1
 }
 
+// Converts a bag of numbers to a slice, with the numbers in order,
+// and repeated the number of times they are in the bag.
 func (bag *IntBag) ToSlice() []int {
 	m := bag.asMap()
 	values := []int{}
@@ -44,6 +50,9 @@ func max(a int, b int) int {
 	}
 }
 
+// Returns the "union" of two bags, where the number of times each
+// number occurs is the maximum of the how many times it occurs in each
+// input bag.
 func union(a *IntBag, b *IntBag) *IntBag {
 	result := map[int]int{}
 	for k, v := range *(a.asMap()) {
